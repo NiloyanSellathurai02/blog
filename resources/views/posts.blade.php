82901cf12f6fd2b9049dd44c155b1f4a5e2aea4f
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-12-14 22:38:48 +0100

    did some back-end work

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,5 +4,17 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-<h1>Hello World</h1>
+    <?php foreach ($posts as $post) :?>
+    <article>
+        <h1>
+            <a href="/posts/<?= $post->slug; ?>">
+                <?= $post->title ?>
+            </a>
+        </h1>
+
+        <p><?=$post->excerpt ?></p>
+    </article>
+
+<?php endforeach ?>
+
 </body>
